55640720bfe0e4a5c2da02c52e57b9dce6af9772
MindZone <mindzonerl@gmail.com>
2024-07-08 20:06:17 +0200

    Added tooltip component

---
+++ src/components/Tooltip.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+
+interface Props {
+    tooltip: string
+}
+
+defineProps<Props>();
+</script>
+
+<template>
+    <TooltipProvider :delay-duration="150">
+        <Tooltip>
+            <TooltipTrigger>
+                <slot/>
+            </TooltipTrigger>
+            <TooltipContent>
+                <p>{{ tooltip }}</p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
+</template>
+
+<style scoped>
+
+</style>
