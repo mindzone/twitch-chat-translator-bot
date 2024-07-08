97589a4be6eb1203578c17490632ffe992199ae8
MindZone <mindzonerl@gmail.com>
2024-07-08 21:55:15 +0200

    Removed placeholder buttons from the dashboard

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,5 +1,4 @@
 <script lang="ts" setup>
-import { Button } from "@/components/ui/button";
 import { useTwitch } from "@/composables/useTwitch.ts";
 import ConnectionStateCard from "@/components/twitch/ConnectionStateCard.vue";
 import DashboardSettingsCard from "@/components/settings/DashboardSettingsCard.vue";
@@ -11,22 +10,6 @@ const twitch = useTwitch();
     <div class="grid grid-cols-2 grid-rows-2 gap-4 p-4 h-screen">
         <ConnectionStateCard/>
         <DashboardSettingsCard class="row-span-2"/>
-
-        <div>
-            Dashboard
-
-            <RouterLink to="/">
-                <Button>
-                    Home
-                </Button>
-            </RouterLink>
-
-            <RouterLink to="/getting-started">
-                <Button>
-                    Get started
-                </Button>
-            </RouterLink>
-        </div>
     </div>
 </template>
 
